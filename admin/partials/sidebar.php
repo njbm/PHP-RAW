4effab1a3491d9b7d36c3ec2a2459c1ee1a14549
jabermasud <njb99m@gmail.com>
2023-05-16 20:55:15 +0600

    update json file

--- admin/partials/sidebar.php
+++ admin/partials/sidebar.php
@@ -1,22 +1,15 @@
 <?php 
-    $navgroups = ['Main','Forms','Components','Layout', 'Data Visualization','Extension','Table','Page Kits'];
-    $mainnavitems = ['Dashboard','Layouts','Themes','Starter Kits','Change Log',' RTL version'];
-    $formnavitems = ['Form Component','JSON Forms', 'Text Editors', 'Pickers'];
-    $componentsnavitems=['Basic Component','Advance Component','Extra Components'];
-    $layoutitems=['Page Layout', 'Sidebar','Navbar','Vertical Navigation','Horizontal Navigation'];
+    $strNavData= file_get_contents($datasource.'adminsidebar.json');
+    $arrNavData= json_decode($strNavData);
 ?>
 
         <!-- Main sidebar -->
 		<div class="sidebar sidebar-light sidebar-main sidebar-expand-md">
 
-
-
-
 			<!-- Sidebar content -->
 			<div class="sidebar-content">
 
             <?php include_once($partials."profile.php"); ?>
-
 
 				<!-- Main navigation -->
 				<div class="card card-sidebar-mobile">
@@ -24,55 +17,82 @@
 
 
 <?php
-    foreach($navgroups as $key=>$navgroup){
+
+    foreach($arrNavData as $navData)
+    {
+        $key=array_keys(get_object_vars($navData))[0];
         echo '<li class="nav-item-header">
                 <div class="text-uppercase font-size-xs line-height-xs">'
-                .$navgroup.
+                . $key .
                 '</div> <i class="icon-menu" title="Main"></i></li>';
-            if($key === 0){
-                foreach($mainnavitems as $mainnavitem){
-                    echo '<li class="nav-item">
-                            <a href="index.html" class="nav-link active">
-                            <i class="icon-home4"></i>
-                        <span> '.$mainnavitem.' </span>
+            foreach($navData->$key as $navItem)
+            {
+                echo '<li class="nav-item">
+                             <a href="slider_index.php" class="nav-link active">
+                             <i class="icon-home4"></i>
+                        <span> '.$navItem.' </span>
                             </a>
-                          </li>';
-                }
-            }
-            if($key === 1){
-                foreach($formnavitems as $formnavitem){
-                    echo '<li class="nav-item">
-                            <a href="index.html" class="nav-link active">
-                            <i class="icon-home4"></i>
-                        <span>
-                            '.$formnavitem.'
-                        </span>
-                    </a>
-                </li>';
-                }
-            }
-            if($key === 2){
-                foreach($componentsnavitems as $componentsnavitem){
-                    echo '<li class="nav-item">
-                            <a href="index.html" class="nav-link active">
-                            <i class="icon-home4"></i>
-                        <span> '. $componentsnavitem.' </span>
-                    </a>
-                </li>';
-                }
-            }
-            
-            if($key === 3){
-                foreach($layoutitems as $layoutitem){
-                    echo '<li class="nav-item">
-                            <a href="index.html" class="nav-link active">
-                            <i class="icon-home4"></i>
-                        <span> '. $layoutitem.' </span>
-                    </a>
-                </li>';
-                }
-            }
+                         </li>';
+            }        
+
     }
+
+
+
+
+
+
+
+
+    // foreach($navgroups as $key=>$navgroup){
+    //     echo '<li class="nav-item-header">
+    //             <div class="text-uppercase font-size-xs line-height-xs">'
+    //             .$navgroup.
+    //             '</div> <i class="icon-menu" title="Main"></i></li>';
+    //         if($key === 0){
+    //             foreach($mainnavitems as $mainnavitem){
+    //                 echo '<li class="nav-item">
+    //                         <a href="index.html" class="nav-link active">
+    //                         <i class="icon-home4"></i>
+    //                     <span> '.$mainnavitem.' </span>
+    //                         </a>
+    //                       </li>';
+    //             }
+    //         }
+    //         if($key === 1){
+    //             foreach($formnavitems as $formnavitem){
+    //                 echo '<li class="nav-item">
+    //                         <a href="index.html" class="nav-link active">
+    //                         <i class="icon-home4"></i>
+    //                     <span>
+    //                         '.$formnavitem.'
+    //                     </span>
+    //                 </a>
+    //             </li>';
+    //             }
+    //         }
+    //         if($key === 2){
+    //             foreach($componentsnavitems as $componentsnavitem){
+    //                 echo '<li class="nav-item">
+    //                         <a href="index.html" class="nav-link active">
+    //                         <i class="icon-home4"></i>
+    //                     <span> '. $componentsnavitem.' </span>
+    //                 </a>
+    //             </li>';
+    //             }
+    //         }
+            
+    //         if($key === 3){
+    //             foreach($layoutitems as $layoutitem){
+    //                 echo '<li class="nav-item">
+    //                         <a href="index.html" class="nav-link active">
+    //                         <i class="icon-home4"></i>
+    //                     <span> '. $layoutitem.' </span>
+    //                 </a>
+    //             </li>';
+    //             }
+    //         }
+    // }
 
 
 ?>
--- admin/slider_index.php
+++ admin/slider_index.php
@@ -1,4 +1,10 @@
 <?php include_once($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'config.php') ?>
+<?php
+    $dataSlides= file_get_contents($datasource. 'slideritems.json');
+    $slides= json_decode($dataSlides);
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -35,7 +41,62 @@
                 <!-- Dashboard content -->
                 <div class="row">
                     <div class="col-xl-12">
-                        Content goes Here
+                                    <!-- Bordered table -->
+				<div class="card">
+					<div class="card-header header-elements-inline">
+						<h5 class="card-title">Slides</h5>
+						<div class="header-elements">
+							<div class="list-icons">
+		                		<a class="list-icons-item" data-action="collapse"></a>
+		                		<a class="list-icons-item" data-action="reload"></a>
+		                		<a class="list-icons-item" data-action="remove"></a>
+		                	</div>
+	                	</div>
+					</div>
+
+					<div class="card-body">
+						 Create | Trash (Delete | Restore) | Download XL | Download PDF | Print View
+					</div>
+
+					<div class="table-responsive">
+						<table class="table table-bordered">
+							<thead>
+								<tr>
+									<th>#</th>
+									<th>Title</th>
+									<th>Src</th>
+									<th>Alt</th>
+									<th>Caption</th>
+									<th>Action</th>
+								</tr>
+							</thead>
+							<tbody>
+
+<?php
+    foreach($slides as $key=>$slide):
+        // if(0 == $key){
+        //  $active = 'active';
+        // }else{
+        //  $active = '';
+        // }
+     ?> 
+
+
+								<tr>
+									<td title="<?=$slide->uuid?>"><?=++$key?></td>
+									<td><?=$slide->title?></td>
+									<td><img src="<?=$slide->src?>" style="width:100px;height:100px"></td>
+									<td><?=$slide->alt?></td>
+									<td><?=$slide->caption?></td>
+									<td> Show | Edit | Delete | Activate/InActive | Copy</td>
+								</tr>
+<?php
+ endforeach
+?>
+							</tbody>
+						</table>
+					</div>
+				</div>
                     </div>
                 </div>
                 <!-- /dashboard content -->
